fc1c68be7282e9e197e9cf6de2ff5e8588339bcd
环球探测 <heqingnju@foxmail.com>
2013-10-17 20:40:59 +0800

    theme , dag ,log, table

--- js/index.js
+++ js/index.js
@@ -3,6 +3,21 @@ function getDataAjax(urlStr) {
         url: urlStr
     })
 }
+function getDagList() {
+    $("#dag-dg").datagrid({
+        url: "service/main.php?action=dag"
+    })
+}
+function getLogList() {
+    $("#log-dg").datagrid({
+        url: "service/main.php?action=log"
+    })
+}
+function getTableList() {
+    $("#table-dg").datagrid({
+        url: "service/main.php?action=table"
+    })
+}
 function drawTotalChart() {
     var chart;
     var dateArr = [];
@@ -136,6 +151,25 @@ function drawTrendChart() {
         }
     });
 }
+function createSummary() {
+    $.ajax({
+        url: "service/main.php?action=summary",
+        dataType: "json",
+        success: function (res) {
+            if (res.success == 1) {
+                var totalSize = (res.totalSize / 1024).toFixed(5);
+                var dagNum = res.dagNum;
+                var logNum = res.logNum;
+                var tableNum = res.tableNum;
+
+                $("#hq-total-table tr:eq(0) td:eq(1)").html(totalSize + "  P");
+                $("#hq-summary-table tr:eq(0) td:eq(1) a").html(dagNum);
+                $("#hq-summary-table tr:eq(1) td:eq(1) a").html(logNum);
+                $("#hq-summary-table tr:eq(2) td:eq(1) a").html(tableNum);
+            }
+        }
+    })
+}
 $(function () {
     $(".right-child").hide();
     $("#summary-div").show();
@@ -143,7 +177,9 @@ $(function () {
         $(".itemSpan").removeClass("onSelect");
         $(this).addClass("onSelect");
     })
+    createSummary();
     $("#choose-summary").on("click", function () {
+        createSummary();
         $(".right-child[id!='summary-div']").fadeOut("slow", function () {
             $("#summary-div").show();
         });
@@ -152,21 +188,51 @@ $(function () {
     $("#choose-task").on("click", function () {
         $(".right-child[id!='taskDiv']").fadeOut("slow", function () {
             $("#taskDiv").show();
-            getDataAjax("service/main.php");
+            getDataAjax("service/main.php?action=task");
         });
     })
 
     $("#choose-trend").on("click", function () {
         drawTrendChart();
-        $(".right-child[id!='trend-graph']").fadeOut("slow", function () {
-            $("#trend-graph").show();
+        $(".right-child[id!='trend-graph-div']").fadeOut("slow", function () {
+            $(".right-child[id='trend-graph-div']").show();
         });
     })
     $("#choose-total-trend").on("click", function () {
         drawTotalChart();
-        $(".right-child[id!='total-graph']").fadeOut("slow", function () {
-            $("#total-graph").show();
+        $(".right-child[id!='total-graph-div']").fadeOut("slow", function () {
+            $(".right-child[id='total-graph-div']").show();
         });
+    })
+    /**
+     * 选择任务、日志、生成表
+     */
+    $("#choose-dag-list").on("click", function () {
+        $(".right-child[id!='dag-div']").fadeOut("slow", function () {
+            $(".right-child[id='dag-div']").show();
+            getDagList();
+        });
+    })
+    $("#dag-a").on("click", function () {
+        $("#choose-dag-list").trigger("click");
+    })
+    $("#choose-log-list").on("click", function () {
+        $(".right-child[id!='log-div']").fadeOut("slow", function () {
+            $(".right-child[id='log-div']").show();
+            getLogList();
+        });
+    })
+    $("#log-a").on("click", function () {
+        $("#choose-log-list").trigger("click");
+    })
+    $("#choose-table-list").on("click", function () {
+        $(".right-child[id!='table-div']").fadeOut("slow", function () {
+            $(".right-child[id='table-div']").show();
+            getTableList();
+        });
+    })
+    $("#table-a").on("click", function () {
+        $("#choose-table-list").trigger("click");
     })
     $("#hq-query-button").on("click", function () {
         var tagName = $("input[name='tag-name']").val().trim();
--- service/main.php
+++ service/main.php
@@ -2,28 +2,124 @@
 require_once("Db.php");
 $db = Db::getInstance();
 $db->createCon();
-
 $page = isset($_POST['page']) ? intval($_POST['page']) : 1;
 $rows = isset($_POST['rows']) ? intval($_POST['rows']) : 10;
 $offset = ($page - 1) * $rows;
+if ($_GET["action"] == "task") {
+    $sql = "select count(*) from input_config input inner join output_config output on input.dagname=output.dagname";
+    $rs = $db->query($sql);
+    $row = mysql_fetch_row($rs);
+    $result["total"] = $row[0];
 
-$sql = "select count(*) from input_config input inner join output_config output on input.dagname=output.dagname";
-$rs = $db->query($sql);
-$row = mysql_fetch_row($rs);
-$result["total"] = $row[0];
 
+    $sql = 'select input.dagname,input.logname,input.log_type,input.log_path,output.output_table,output.table_type,output.table_path' .
+        ' from input_config input inner join output_config output on input.dagname=output.dagname limit ' . $offset . ',' . $rows . ';';
 
-$sql = 'select input.dagname,input.logname,input.log_type,input.log_path,output.output_table,output.table_type,output.table_path'.
- ' from input_config input inner join output_config output on input.dagname=output.dagname limit '. $offset.','.$rows.';';
+    $rs = $db->query($sql);
+    $resultRows = array();
 
-$rs = $db->query($sql);
-$resultRows = array();
+    while ($row = mysql_fetch_object($rs)) {
+        array_push($resultRows, $row);
+    }
+    $result["rows"] = $resultRows;
+    echo json_encode($result);
+} elseif ($_GET["action"] == "summary") {
+    $rs = $db->query("select distinct date from udw_size order by date desc limit 1;");
+    $row = mysql_fetch_row($rs);
+    $date = $row[0];
 
-while ($row = mysql_fetch_object($rs)) {
-    array_push($resultRows, $row);
+    $rs = $db->query('select sum(size) from udw_size where date="' . $date . '";');
+    $row = mysql_fetch_row($rs);
+    $result["totalSize"] = $row[0];
+
+    $rs = $db->query('select count(distinct dagname) from input_config;');
+    $row = mysql_fetch_row($rs);
+    $result["dagNum"] = $row[0];
+
+    $rs = $db->query('select count(distinct log_path) from input_config;');
+    $row = mysql_fetch_row($rs);
+    $result["logNum"] = $row[0];
+
+    $rs = $db->query('select count(distinct table_path) from output_config;');
+    $row = mysql_fetch_row($rs);
+    $result["tableNum"] = $row[0];
+
+    $result["success"] = 1;
+    echo json_encode($result);
+} elseif ($_GET["action"] == "dag") {
+    $sql = "select COUNT(DISTINCT dagname) FROM input_config;";
+    $rs = $db->query($sql);
+    $row = mysql_fetch_row($rs);
+    $result["total"] = $row[0];
+
+    $sql = 'select DISTINCT dagname FROM input_config limit ' . $offset . ',' . $rows . ';';
+
+    $rs = $db->query($sql);
+    $resultRows = array();
+
+    while ($row = mysql_fetch_array($rs)) {
+        $newRow["dagname"] = $row[0];
+        $tableRs = $db->query('select output_table FROM output_config WHERE dagname="' . $row[0] . '"  LIMIT 1;');
+        $tableRow = mysql_fetch_array($tableRs);
+        $table = $tableRow[0];
+        $productRs = $db->query('select product FROM product_bigtable WHERE tablename="' . $table . '" UNION select product FROM product_smalltable WHERE tablename="' . $table . '";');
+        $productRow = mysql_fetch_array($productRs);
+        $newRow["product"] = $productRow[0];
+        array_push($resultRows, $newRow);
+    }
+    $result["rows"] = $resultRows;
+    echo json_encode($result);
+} elseif ($_GET["action"] == "log") {
+    $sql = "select COUNT(DISTINCT log_path) FROM input_config;";
+    $rs = $db->query($sql);
+    $row = mysql_fetch_row($rs);
+    $result["total"] = $row[0];
+
+    $sql = 'select DISTINCT log_path FROM input_config limit ' . $offset . ',' . $rows . ';';
+
+    $rs = $db->query($sql);
+    $resultRows = array();
+
+    while ($row = mysql_fetch_array($rs)) {
+        $dagRs = $db->query('select dagname,logname,log_type,log_path FROM input_config WHERE log_path="' . $row[0] . '"  LIMIT 1;');
+        $dagRow = mysql_fetch_array($dagRs);
+        $newRow["logname"] = $dagRow[1];
+        $newRow["logtype"] = $dagRow[2];
+        $newRow["logpath"] = $dagRow[3];
+        $tableRs = $db->query('select output_table FROM output_config WHERE dagname="' . $dagRow[0] . '"  LIMIT 1;');
+        $tableRow = mysql_fetch_array($tableRs);
+        $table = $tableRow[0];
+        $productRs = $db->query('select product FROM product_bigtable WHERE tablename="' . $table . '" UNION select product FROM product_smalltable WHERE tablename="' . $table . '";');
+        $productRow = mysql_fetch_array($productRs);
+        $newRow["product"] = $productRow[0];
+        array_push($resultRows, $newRow);
+    }
+    $result["rows"] = $resultRows;
+    echo json_encode($result);
+} elseif ($_GET["action"] == "table") {
+    $sql = "select COUNT(DISTINCT table_path) FROM output_config;";
+    $rs = $db->query($sql);
+    $row = mysql_fetch_row($rs);
+    $result["total"] = $row[0];
+
+    $sql = 'select DISTINCT table_path FROM output_config limit ' . $offset . ',' . $rows . ';';
+
+    $rs = $db->query($sql);
+    $resultRows = array();
+
+    while ($row = mysql_fetch_array($rs)) {
+        $tableRs = $db->query('select output_table,table_type,table_path FROM output_config WHERE table_path="' . $row[0] . '"  LIMIT 1;');
+        $tableRow = mysql_fetch_array($tableRs);
+        $newRow["tablename"] = $tableRow[0];
+        $newRow["tabletype"] = $tableRow[1];
+        $newRow["tablepath"] = $tableRow[2];
+        $productRs = $db->query('select product FROM product_bigtable WHERE tablename="' . $tableRow[0] . '" UNION select product FROM product_smalltable WHERE tablename="' . $tableRow[0] . '";');
+        $productRow = mysql_fetch_array($productRs);
+        $newRow["product"] = $productRow[0];
+        array_push($resultRows, $newRow);
+    }
+    $result["rows"] = $resultRows;
+    echo json_encode($result);
 }
-
-$result["rows"] = $resultRows;
-echo json_encode($result);
 $db->close();
 	
